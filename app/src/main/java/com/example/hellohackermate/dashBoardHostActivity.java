c9c81085264a786c9b46671700ed6df41c1acdbc
vishal R <vishal206ani@gmail.com>
2021-07-24 14:36:57 +0530

    completed bottom navigation

--- app/src/main/java/com/example/hellohackermate/dashBoardHostActivity.java
+++ app/src/main/java/com/example/hellohackermate/dashBoardHostActivity.java
@@ -1,14 +1,85 @@
 package com.example.hellohackermate;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class dashBoardHostActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    String myuid;
+    ActionBar actionBar;
+    BottomNavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board_host);
+
+        actionBar =getSupportActionBar();
+        actionBar.setTitle("Profile Activity");
+
+        firebaseAuth=FirebaseAuth.getInstance();
+
+        navigationView =findViewById(R.id.navigation);
+        navigationView.setOnNavigationItemSelectedListener(selectedListener);
+        actionBar.setTitle("Home");
+
+        HomeFragment fragment=new HomeFragment();
+        FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.content,fragment,"");
+        fragmentTransaction.commit();
+        
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener selectedListener=new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+            switch (menuItem.getItemId()){
+                case R.id.nav_home:
+                    actionBar.setTitle("Home");
+                    HomeFragment fragment=new HomeFragment();
+                    FragmentTransaction fragmentTransaction=getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.content,fragment,"");
+                    fragmentTransaction.commit();
+                    return true;
+
+                case R.id.nav_profile:
+                    actionBar.setTitle("Profile");
+                    ProfileFragment fragment1=new ProfileFragment();
+                    FragmentTransaction fragmentTransaction1=getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction1.replace(R.id.content,fragment1,"");
+                    fragmentTransaction1.commit();
+                    return true;
+
+                case R.id.nav_users:
+                    actionBar.setTitle("Users");
+                    UsersFragment fragment2=new UsersFragment();
+                    FragmentTransaction fragmentTransaction2=getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction2.replace(R.id.content,fragment2,"");
+                    fragmentTransaction2.commit();
+                    return true;
+
+                case R.id.nav_addevent:
+                    actionBar.setTitle("Add event");
+                    AddEventFragment fragment3=new AddEventFragment();
+                    FragmentTransaction fragmentTransaction3=getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction3.replace(R.id.content,fragment3,"");
+                    fragmentTransaction3.commit();
+                    return true;
+            }
+            return false;
+        }
+    };
 }
